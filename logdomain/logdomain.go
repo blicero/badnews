f646ecd1c01c801a1393f3345fb1f19bc095003d
Benjamin Walkenhorst <krylon@posteo.de>
2024-11-04 19:26:57 +0100

    Add Logdomain for Blacklist

--- logdomain/logdomain.go
+++ logdomain/logdomain.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 18. 09. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-10-22 15:49:21 krylon>
+// Time-stamp: <2024-11-04 17:53:24 krylon>
 
 package logdomain
 
@@ -17,6 +17,7 @@ const (
 	Reader
 	Web
 	Advisor
+	Blacklist
 )
 
 func AllDomains() []ID {
@@ -27,5 +28,6 @@ func AllDomains() []ID {
 		Reader,
 		Web,
 		Advisor,
+		Blacklist,
 	}
 } // func AllDomains() []ID
