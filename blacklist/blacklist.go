e378cde96aca204409ee9852ab6632a85d2e6bdd
Benjamin Walkenhorst <krylon@posteo.de>
2024-11-04 19:25:35 +0100

    Blacklist: Add some logging, fix singleton handling

--- blacklist/blacklist.go
+++ blacklist/blacklist.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 01. 11. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-11-02 21:42:11 krylon>
+// Time-stamp: <2024-11-04 18:48:37 krylon>
 
 // Package blacklist provides a way to filter news Items with regular expressions.
 package blacklist
@@ -12,12 +12,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"regexp"
 	"sort"
 	"sync"
 	"sync/atomic"
 
+	"github.com/blicero/badnews/common"
+	"github.com/blicero/badnews/logdomain"
 	"github.com/blicero/badnews/model"
 )
 
@@ -77,8 +80,10 @@ func (p *Pattern) UnmarshalJSON(b []byte) error {
 
 // Blacklist is a collection of Patterns.
 type Blacklist struct {
-	lock sync.RWMutex
-	List []*Pattern
+	lock    sync.RWMutex
+	log     *log.Logger
+	changed atomic.Bool
+	List    []*Pattern
 }
 
 var (
@@ -86,71 +91,60 @@ var (
 	openLock sync.Mutex
 )
 
-// New creates a new Blacklist from the given list of patterns
-func New(pattern ...string) (*Blacklist, error) {
-	var (
-		err error
-		bl  *Blacklist
-	)
-
-	openLock.Lock()
-	defer openLock.Unlock()
-
-	if instance != nil {
-		return instance, nil
-	}
-
-	bl = &Blacklist{
-		List: make([]*Pattern, len(pattern)),
-	}
-	instance = bl
-
-	for idx, pat := range pattern {
-		var p = &Pattern{
-			ID: int64(idx) + 1,
-		}
-
-		if p.Pattern, err = regexp.Compile(pat); err != nil {
-			return nil, err
-		}
-
-		bl.List[idx] = p
-	}
-
-	return bl, nil
-} // func New(pattern... string) (Blacklist, error)
-
 // NewFromFile restores a Blacklist from a JSON file.
 func NewFromFile(path string) (*Blacklist, error) {
 	var (
-		err error
-		bl  *Blacklist = new(Blacklist)
-		fh  *os.File
-		buf bytes.Buffer
+		err    error
+		bl     *Blacklist = new(Blacklist)
+		logger *log.Logger
+		fh     *os.File
+		buf    bytes.Buffer
 	)
 
 	openLock.Lock()
 	defer openLock.Unlock()
 
 	if instance != nil {
-		return bl, nil
+		instance.log.Println("[DEBUG] Use existing Blacklist Singleton")
+		return instance, nil
+	} else if logger, err = common.GetLogger(logdomain.Blacklist); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Failed to create Logger for Blacklist: %s\n",
+			err.Error(),
+		)
+		return nil, err
 	}
+
+	logger.Printf("[INFO] Restore Blacklist from %s\n",
+		path)
 
 	if fh, err = os.Open(path); err != nil {
 		if os.IsNotExist(err) {
+			bl.List = make([]*Pattern, 0, 8)
+			bl.log = logger
 			return bl, nil
 		}
+		logger.Printf("[ERROR] Cannot open blacklist dump at %s: %s\n",
+			path,
+			err.Error())
 		return nil, err
 	}
 
 	defer fh.Close() // nolint: errcheck
 
 	if _, err = io.Copy(&buf, fh); err != nil {
+		logger.Printf("[ERROR] Failed to load serialized Blacklist from %s: %s\n",
+			path,
+			err.Error())
 		return nil, err
 	} else if err = json.Unmarshal(buf.Bytes(), &bl); err != nil {
+		logger.Printf("[ERROR] Failed to de-serialize Blacklist: %s\n",
+			err.Error())
 		return nil, err
 	}
 
+	bl.log = logger
 	instance = bl
 	return bl, nil
 } // func NewFromFile(path string) (Blacklist, error)
@@ -158,6 +152,10 @@ func NewFromFile(path string) (*Blacklist, error) {
 func (bl *Blacklist) Len() int           { return len(bl.List) }
 func (bl *Blacklist) Swap(i, j int)      { bl.List[i], bl.List[j] = bl.List[j], bl.List[i] }
 func (bl *Blacklist) Less(i, j int) bool { return bl.List[i].Cnt.Load() > bl.List[j].Cnt.Load() }
+
+// Changed returns the Blacklist's change flag. A return value of true indicates the
+// contents of the Blacklist have changed since it was created / last saved to disk.
+func (bl *Blacklist) Changed() bool { return bl.changed.Load() }
 
 // Match checks is the given Item is matched by any of the patterns in the Blacklist.
 // If a match is found, the list is sorted.
@@ -167,6 +165,10 @@ func (bl *Blacklist) Match(i *model.Item) bool {
 
 	for _, p := range bl.List {
 		if p.Match(i) {
+			bl.log.Printf("[DEBUG] Blacklist Item %q matches Item %q\n",
+				p.Pattern,
+				i.Headline)
+			bl.changed.Store(true)
 			return true
 		}
 	}
@@ -174,9 +176,13 @@ func (bl *Blacklist) Match(i *model.Item) bool {
 	return false
 } // func (bl Blacklist) Match(i *model.Item) bool
 
+// Sort sorts the Patterns of the Blacklist so that Patterns with larger match
+// counts move to the front.
 func (bl *Blacklist) Sort() {
 	bl.lock.Lock()
 	defer bl.lock.Unlock()
+
+	bl.log.Println("[DEBUG] Sorting Blacklist")
 
 	sort.Sort(bl)
 } // func (bl *Blacklist) Sort()
@@ -194,9 +200,17 @@ func (bl *Blacklist) Dump(path string) error {
 	bl.lock.RLock()
 	defer bl.lock.RUnlock()
 
+	bl.log.Printf("[INFO] Dumping Blacklist to %s\n",
+		path)
+
 	if data, err = json.Marshal(bl); err != nil {
+		bl.log.Printf("[ERROR] Failed to serialize Blacklist: %s\n",
+			err.Error())
 		return err
 	} else if fh, err = os.Create(path); err != nil {
+		bl.log.Printf("[ERROR] Failed to open Blacklist file at %s: %s\n",
+			path,
+			err.Error())
 		return err
 	}
 
@@ -205,8 +219,13 @@ func (bl *Blacklist) Dump(path string) error {
 	buf = bytes.NewBuffer(data)
 
 	if _, err = io.Copy(fh, buf); err != nil {
+		bl.log.Printf("[ERROR] Failed to write serialized Blacklist to %s: %s\n",
+			path,
+			err.Error())
 		return err
 	}
+
+	bl.changed.Store(false)
 
 	return nil
 } // func (bl Blacklist) Dump(path string) error
@@ -215,6 +234,7 @@ func (bl *Blacklist) Dump(path string) error {
 func (bl *Blacklist) Add(p *Pattern) {
 	bl.lock.Lock()
 	bl.List = append(bl.List, p)
+	bl.changed.Store(true)
 	bl.lock.Unlock()
 }
 
@@ -229,6 +249,7 @@ func (bl *Blacklist) AddString(s string) error {
 
 	bl.lock.Lock()
 	defer bl.lock.Unlock()
+	bl.changed.Store(true)
 	bl.List = append(bl.List, p)
 	return nil
 } // func (bl *Blacklist) AddString(s string) error
