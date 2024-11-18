18cd230c69e47ad2c3bd3d6e1dbdad6adfc8dd53
Benjamin Walkenhorst <krylon@posteo.de>
2024-11-18 20:48:00 +0100

    Temporarily disable deleting the test folder for package database

--- database/00_db_main_test.go
+++ database/00_db_main_test.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 20. 09. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-10-12 19:49:37 krylon>
+// Time-stamp: <2024-11-18 20:25:10 krylon>
 
 package database
 
@@ -47,9 +47,9 @@ func TestMain(m *testing.M) {
 		// database inside it) around, so we can manually inspect it
 		// if needed.
 		// If all tests pass, OTOH, we can safely remove the directory.
-		fmt.Printf("Removing BaseDir %s\n",
+		fmt.Printf("NOT Removing BaseDir %s\n",
 			baseDir)
-		_ = os.RemoveAll(baseDir)
+		// _ = os.RemoveAll(baseDir)
 	} else {
 		fmt.Printf(">>> TEST DIRECTORY: %s\n", baseDir)
 	}
