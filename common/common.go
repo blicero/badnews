14bbcba1caf343ad84badf5389ea8eed60432a09
Benjamin Walkenhorst <krylon@posteo.de>
2024-12-04 18:11:44 +0100

    Bump version number to 0.14.0

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 18. 09. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-11-12 20:02:55 krylon>
+// Time-stamp: <2024-12-04 18:11:38 krylon>
 
 package common
 
@@ -36,7 +36,7 @@ import (
 // their status.
 const (
 	Debug                    = true
-	Version                  = "0.13.0"
+	Version                  = "0.14.0"
 	AppName                  = "BadNews"
 	TimestampFormat          = "2006-01-02 15:04:05"
 	TimestampFormatMinute    = "2006-01-02 15:04"
